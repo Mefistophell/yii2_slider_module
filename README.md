23c5bdc6eb5a1fae378b72d65080cf2bba6a396f
Mefistophell <lovecraft.crowley@gmail.com>
2014-11-13 14:11:07 +0200

    Add migrations

--- README.md
+++ README.md
@@ -55,6 +55,10 @@ Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 
 - Create a new table in your database from file: `yii2_start_slider.sql`
 
+OR: Apply migration with console commands:
+
+`php yii migrate --migrationPath=@nill/slider/migrations`
+
 - RBAC settings: For usage modules create Role "administrateSlider"
 
 ```
--- yii2_start_slider.sql
+++ yii2_start_slider.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Хост: 127.0.0.1
--- Время создания: Ноя 13 2014 г., 13:29
--- Версия сервера: 5.5.25
--- Версия PHP: 5.5.9
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- База данных: `yii2demo`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `yii2_start_slider`
---
-
 CREATE TABLE IF NOT EXISTS `yii2_start_slider` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `h1` text NOT NULL,
@@ -35,7 +7,3 @@ CREATE TABLE IF NOT EXISTS `yii2_start_slider` (
   `img` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
